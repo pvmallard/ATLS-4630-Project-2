a4ef35c04f6a964c7cbcd5d2fc74dcf803a69ded
Mallory Prescott <mvprescott17@gmail.com>
2024-12-03 18:37:35 -0700

    Milestone 2 Progress

--- Are-You-Smarter-Than-Peter/src/App.jsx
+++ Are-You-Smarter-Than-Peter/src/App.jsx
@@ -1,12 +1,30 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Start from './Start.jsx'
-// import Result from './Result.jsx'
-// import Question from './Question.jsx'
+import Result from './Result.jsx'
+import Question from './Question.jsx'
 
 export default function App() {
+
+  const [state, updateState] = useState("question")
+
+  // function nextQuestion(){
+  //   if end of list call update state("")
+  // }
+
   return (
     <div>
-    <Start />
+      <h2>hello</h2>
+      {state == "start" &&
+        <Start />
+      }
+
+      {state == "question" &&
+        <Question />
+      }
+
+      {state == "result" &&
+        <Result />
+      }
     </div>
   );
 }
